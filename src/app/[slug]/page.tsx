c80ec139ddcd51ac8d2591d264edafc90a3d5724
foqsi <foqsi03@gmail.com>
2025-06-28 18:55:17 -0500

    asdftsdfhgdgrfsjnkfugj

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,19 +1,19 @@
-import { getTenantBySlug } from '@/lib/tenants';
-import { notFound } from 'next/navigation';
+// import { getTenantBySlug } from '@/lib/tenants';
+// import { notFound } from 'next/navigation';
 
-interface PageProps {
-  params: { slug: string };
-}
+// interface PageProps {
+//   params: { slug: string };
+// }
 
-export default async function TenantSite({ params }: PageProps) {
-  const tenant = await getTenantBySlug(params.slug);
+// export default async function TenantSite({ params }: PageProps) {
+//   const tenant = await getTenantBySlug(params.slug);
 
-  if (!tenant) return notFound();
+//   if (!tenant) return notFound();
 
-  return (
-    <div>
-      <h1>Welcome to {tenant.companyName}</h1>
-      <p>{tenant.about}</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Welcome to {tenant.companyName}</h1>
+//       <p>{tenant.about}</p>
+//     </div>
+//   );
+// }
